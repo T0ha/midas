febd404fa07857256513ac978ebca5c670e122bc
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-11-29 14:08:04 +0500

    sensitive data removed from get_portfolio

--- python/get_portfolio.py
+++ python/get_portfolio.py
@@ -2,22 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from datetime import date
+from settings import API_KEY, SECRET
 from binance.spot import Spot as Client
 import pandas as pd
 
-API_KEY = "wbG3iHSZiOAoEh0K5T5ZQ9RFBgVQdSHTXOcd4WumMNYaGdDytNf0RZvfoU2DPm9C"
-SECRET = "o8Q6Sy1Ioi3MoLwcKR7DUMKzLYlV2sgEyRYoY1ZdSK9wNCrGEm0HTFu5lKqUytli"
 
 def main():
     client = Client(API_KEY, SECRET)
-    #await client.load()
 
     portfolio = client.account()
     print(portfolio)
     portfolio_pd = pd.DataFrame(portfolio['balances'], dtype='float')
     portfolio_pd = portfolio_pd.set_index(portfolio_pd['asset'])
     print(portfolio_pd.loc[(portfolio_pd['free'] != 0)])
-    #await client.close()
 
 if __name__ == "__main__":
     main()
