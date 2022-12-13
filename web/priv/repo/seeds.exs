fe68e84148414b4d20f7c357f5cae64385cbf3df
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-12-13 20:05:45 +0500

    Wallets for Binance added to seeds

--- web/priv/repo/seeds.exs
+++ web/priv/repo/seeds.exs
@@ -10,9 +10,37 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 alias Portfolio.Accounts.User
+alias Portfolio.Wallets.Wallet
+
 %User{}
 |> User.registration_changeset(%{
   email: "t0hashvein@gmail.com",
   password: "G00dP@ssw0rd"
+})
+|> Portfolio.Repo.insert!()
+
+%Wallet{}
+|> Wallet.changeset(%{
+  id: 1,
+  name: "Binance",
+  type: 2
+})
+|> Portfolio.Repo.insert!()
+
+%Wallet{}
+|> Wallet.changeset(%{
+  id: 2,
+  name: "Spot",
+  type: 2,
+  parent_id: 1
+})
+|> Portfolio.Repo.insert!()
+
+%Wallet{}
+|> Wallet.changeset(%{
+  id: 3,
+  name: "Earn",
+  type: 2,
+  parent_id: 1
 })
 |> Portfolio.Repo.insert!()
