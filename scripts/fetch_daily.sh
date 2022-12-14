cb28544bbd54c236e7c17ba5260c3258a1454ba3
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-12-14 14:42:06 +0500

    Added logging to daily script

--- scripts/fetch_daily.sh
+++ scripts/fetch_daily.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-COMPOSE_CONFIG="/home/t0ha/projects/midas/compose.yml"
+PROJECT_PATH="/home/t0ha/projects/midas"
+COMPOSE_CONFIG="${PROJECT_PATH}/compose.yml"
 CMD="docker compose -f ${COMPOSE_CONFIG} run spider python"
 
-${CMD} get_prices_db.py
-${CMD} get_portfolio_db.py
+${CMD} get_prices_db.py >> ${PROJECT_PATH}/daily.log 2>&1
+${CMD} get_portfolio_db.py >> ${PROJECT_PATH}/daily.log 2>&1
