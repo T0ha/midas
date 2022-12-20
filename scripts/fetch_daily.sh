7c9c6061d2995d0082749bacf8a72823b5943b46
Anton Shvein aka T0ha <t0hashvein@gmail.com>
2022-12-20 15:34:02 +0500

    Fetch order fixed for trigger

--- scripts/fetch_daily.sh
+++ scripts/fetch_daily.sh
@@ -4,5 +4,5 @@ PROJECT_PATH="/home/t0ha/projects/midas"
 COMPOSE_CONFIG="${PROJECT_PATH}/compose.yml"
 CMD="docker compose -f ${COMPOSE_CONFIG} run spider python"
 
-${CMD} get_prices_db.py >> ${PROJECT_PATH}/daily.log 2>&1
 ${CMD} get_portfolio_db.py >> ${PROJECT_PATH}/daily.log 2>&1
+${CMD} get_prices_db.py >> ${PROJECT_PATH}/daily.log 2>&1
